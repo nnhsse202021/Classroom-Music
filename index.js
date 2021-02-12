aa0dcb771a2249fd897aa2ce395298857c8ef4b1
Geoffrey Wu <geoffrey.feifei@gmail.com>
2021-02-12 17:58:24 +0000

    Set up web server with youtube API calls

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const bodyParser = require('body-parser')
-const axios = require("axios");
+const axios = require("axios"); // for requests (essentially equivalent to the fetch command)
 
+// For storing the API key in the .env file and retrieving it
 const dotenv = require("dotenv")
 dotenv.config()
 
-var key = process.env.KEY
-
-console.log(process.env.KEY)
+//var key = process.env.KEY // don't need this at the moment, would rather not store the key value
 
 app.use(bodyParser.json())
 
 // '/' endpoint handling
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // Return the html file for the home directory of the server
   res.sendFile(__dirname + '/static/index.html');
+});
 
-  let keyword = "Fortnite"
-  let url = "https://www.googleapis.com/youtube/v3/search?key=" + process.env.KEY + "&type=video&part=snippet&maxResults=3&q=" + keyword;
-  axios.get(url)
+app.get("/ytapi", (req,res) => {
+
+  let keyword = req.query.term;
+  console.log("Search Term:  " + keyword); // temporary logging
+
+  axios.get("https://www.googleapis.com/youtube/v3/search", { // Axios request
+    params: {
+      key: process.env.KEY, // Our API key
+      type: "video", // type of youtube content
+      part: "snippet",
+			videoCategoryId: 10, //only returns videos that are categorized as songs
+      maxResults: 3, // amount of results provided
+      q: keyword // search term(s)
+    }
+  })
   .then(response => {
+    // iterating through the JSON and getting the data we want from it
+    
     for(var i = 0; i < response.data.items.length; i++) {
       item = response.data.items[i];
-			console.log("[%s] Title: %s", item.id.videoId, item.snippet.title);
+			console.log("[%s] Title: %s", item.id.videoId, item.snippet.title.replace("&#39;", "'"));
     }
+    res.send(JSON.stringify({
+      videoId1: response.data.items[0].id.videoId,
+      videoTitle1: response.data.items[0].snippet.title,
+      videoId2: response.data.items[1].id.videoId,
+      videoTitle2: response.data.items[1].snippet.title,
+      videoId3: response.data.items[2].id.videoId,
+      videoTitle3: response.data.items[2].snippet.title
+    }));
   })
   .catch(error => {
     console.log(error);
   })
 
-});
-
-
-
+})
 
 //serving files
 app.use((req, res) => {
@@ -50,8 +69,6 @@ http.listen(3000, function(){
 // GET
 // retrieving resources from servers
 
+
 // POST
 // sending data to servers
-app.post("/api/endpoint", (req, res) => {
-  console.log("got a post request");
-})
--- static/script.js
+++ static/script.js
@@ -1,56 +1,25 @@
 // https://github.com/google/google-api-javascript-client
 
-function submitWord() {
+let submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", async () => {
 	let searchTerm = document.getElementById("searchWord").value;
+  document.getElementById("termDisplay").innerHTML = "Search Term: " + searchTerm;
 	//alert(searchTerm);
-  fetch("https://express-base.evanzimmerman.repl.co/api/endpoint", {
-    method: "POST",
-    body: searchTerm
-  }).then(result => result.json()).then(console.log)
-}
 
-
-
-
-/*
-var key = process.env.KEY
-
-function showResponse(response) {
-    var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('response').innerHTML += responseString;
-}
-
-// Called automatically when JavaScript client library is loaded.
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-// Called automatically when YouTube API interface is loaded (see line 9).
-function onYouTubeApiLoad() {
-    // This API key is intended for use only in this lesson.
-    // See https://goo.gl/PdPA1 to get a key for your own applications.
-    let key = process.env.KEY;
-    gapi.client.setApiKey(key);
-
-    search();
-}
-
-function search() {
-    // Use the JavaScript client library to create a search.list() API call.
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: "funny cats"
-        
+  await fetch(`/ytapi?term=${encodeURI(searchTerm)}`)
+    .then(res => res.json())
+    .then( json => {
+      for (var i = 0; i < response.data.items.length; i++) {
+      item = response.data.items[i];
+			console.log("[%s] Title: %s", item.id.videoId, item.snippet.title);
+      }
     });
-    
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
-}
 
-// Called automatically with the response of the YouTube API request.
-function onSearchResponse(response) {
-    showResponse(response);
-}
+})
 
-*/
+let cancelButton = document.getElementById("cancelButton");
+
+cancelButton.addEventListener("click", async () => {
+	document.getElementById("searchWord").value = "";
+})
