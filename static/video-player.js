241a9f6e35e4ad883432eaa521fb26287f6ed198
Geoffrey Wu <geoffrey.feifei@gmail.com>
2021-04-07 18:34:30 -0500

    code refactoring

---
+++ static/video-player.js
@@ -0,0 +1,33 @@
+
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '300', // I changed this to 1 (from 300) to make the player invisible
+    width: '300',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+
+}
+
+// 5. The API calls this function when the player's state changes.
+var done = false;
+function onPlayerStateChange(event) {
+
+}
