48e8cb76e3b4232f8e0035c93aa1315c6b2cc010
Geoffrey Wu <geoffrey.feifei@gmail.com>
2021-04-07 15:42:29 -0500

    added teacher formatting

--- static/teacher.js
+++ static/teacher.js
@@ -23,92 +23,50 @@ async function deletePlaylist(playlistID) {
   return playlist;
 }
 
-// returns the current class code for the teacher
-async function getCurrentCode() {
-  var email = profile.getEmail();
-  var code;
-  await fetch(`/generatecode?email=${encodeURI(email)}`)
-    .then(response => response.json())
-    .then(data => {
-      code = data.code;
-    });
-  return code;
-}
-
-async function getClassList(code) {
-	var classroom;
-  await fetch(`/getclass?code=${encodeURI(code)}`)
-    .then(response => response.json())
-    .then(data => {
-      classroom = data.classroom;
-    })
-  return classroom;
-}
 
 var currentSongIndex = 0;
 document.getElementById("loadButton").addEventListener("click", async () => {
-	if (player) {
-		var playlist = await getPlaylist(await getCurrentCode());
-		player.loadVideoById(playlist.split(",")[currentSongIndex]);
-		currentSongIndex+=2;
-	}
+  if (player) {
+    var playlist = await getPlaylist("tea");
+    player.loadVideoById(playlist.split(",")[currentSongIndex]);
+    currentSongIndex += 2;
+  }
 });
 
 
-var isPlaying = false;
 document.getElementById("playButton").addEventListener("click", () => {
-  if (isPlaying == true) { // Execute this if a video is currently playing
-    player.pauseVideo();
-    document.getElementById("playButtonText").innerHTML = "Play";
-  } else { // Execute this if a video is currently paused
-    player.playVideo();
-    document.getElementById("playButtonText").innerHTML = "Pause";
-  }
+  player.playVideo();
+});
 
-	isPlaying = !isPlaying;
+
+document.getElementById("pauseButton").addEventListener("click", () => {
+  player.pauseVideo();
 });
 
 
 document.getElementById("showPlaylist").addEventListener("click", async () => {
-	document.getElementById("playlist").innerHTML = '';
-	var playlist = await getPlaylist(await getCurrentCode());
-	if(playlist === null) {
-		return;
-	}
-	var songs = playlist.split(',');
-	console.log(songs);
+  document.getElementById("playlist").innerHTML = '';
+  var playlist = await getPlaylist("tea");
+  if (playlist === null) {
+    return;
+  }
+  var songs = playlist.split(',');
+  console.log(songs);
   // song id
-	for(i = 0; i < songs.length; i+=2) {
-    var stuName = songs[i+1];
-		await fetch(`/videoidtotitle?id=${encodeURI(songs[i])}`)
-			.then(response => response.json())
-			.then(data => {
-				var newItem = document.createElement('li');
-				newItem.innerHTML = data.title;
-				document.getElementById('playlist').appendChild(newItem).append(" ----------- Submitted by: " + stuName);
-			});
-	}
+  for (i = 0; i < songs.length; i += 2) {
+    var stuName = songs[i + 1];
+    await fetch(`/videoidtotitle?id=${encodeURI(songs[i])}`)
+      .then(response => response.json())
+      .then(data => {
+        var newItem = document.createElement('li');
+        newItem.innerHTML = data.title;
+        document.getElementById('playlist').appendChild(newItem).append(" ----------- Submitted by: " + stuName);
+      });
+  }
 });
 
 
 document.getElementById("clearPlaylist").addEventListener("click", async () => {
-  deletePlaylist(await getCurrentCode());
+  deletePlaylist("tea");
   window.alert("Playlist has been cleared!");
-})
-
-
-document.getElementById("generateCode").addEventListener("click", async () => {
-  var email = profile.getEmail();
-  document.getElementById("displayCode").innerHTML = "Your code is: " + await getCurrentCode();
-})
-
-document.getElementById("refreshClass").addEventListener("click", async () => {
-  let classroom = await getClassList(await getCurrentCode());
-	document.getElementById('class-list').innerHTML = "";
-
-	for (let i = 0; i < classroom.length; i++) {
-		let newItem = document.createElement('li');
-		newItem.innerHTML = classroom[i];
-		document.getElementById('class-list').appendChild(newItem);
-	}
 })
